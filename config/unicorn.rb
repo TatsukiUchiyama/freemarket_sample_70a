a930a9de770cb4a2ca4bd1347f914eb07eb247d6
NakajimaDctr <drcardhms@gmail.com>
2020-03-05 18:34:39 +0900

    【デプロイ作業】unicorn.rb修正（listen修正）

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-"#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
@@ -21,8 +21,6 @@ stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
-
-listen "#{app_path}/tmp/sockets/unicorn.sock"
 
 #以下は応用的な設定なので説明は割愛
 
