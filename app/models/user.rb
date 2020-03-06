d3576f039c67012606d65532839d908fe5238c01
nakamurayuuma <yuma1231580@gmail.com>
2020-03-06 18:38:48 +0900

    ユーザー新規登録等　
    ユーザー部分のみのバリデーション実装を行っている。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :address
+  has_many :products
+  has_many :cards
+  has_many :transactions
 
-         validates :nickname,:family_name_kanji,:first_name_kanji,:family_name_kana,:first_name_kana,:birthday_year,:birthday_month,:birthday_day ,presence: true
+
+  validates :nickname,:birthday_year,:birthday_month,:birthday_day , presence: true
          
- 
+  validates :email, format: { with: /\A[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]\z/i}
+  
+  validates :family_name_kanji, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]\z/i}
+
+  validates :first_name_kanji, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]\z/i}
+
+  validates :family_name_kana, presence: true,
+    format: { with: /\A[^[ァ-ー]+$]\z/i}
+
+  validates :first_name_kana, presence: true,
+    format: { with: /\A[^[ァ-ー]+]$\z/i}
+
+  validates :encrypted_password, confirmation: true,
+    length: { minimum: 7, maximum: 100} 
 end
