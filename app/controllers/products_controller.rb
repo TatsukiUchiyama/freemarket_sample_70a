62b1e1b3714b937391f12868af95b5534456be77
higasimaru <andyourbirdcansing@icloud.com>
2020-03-09 18:23:25 +0900

    ボタン作成
    購入、編集、削除ボタン作成。
    遷移先指定。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,12 @@ class ProductsController < ApplicationController
   def show   
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
   resources :products
-  resources :transactions, only: :new
+  resources :transactions, only: [:new, :show, :edit, :destroy]
 end
