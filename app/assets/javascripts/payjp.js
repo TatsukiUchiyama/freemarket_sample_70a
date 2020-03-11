5677a8ae1b7a014f4c7fea02c4063bc9513f882d
NakajimaDctr <drcardhms@gmail.com>
2020-03-11 13:04:15 +0900

    payjp.jsのコメント修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,29 +1,54 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
-    if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_647e9e364c53fd7162520df4"); //ここに公開鍵を直書き
-      let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
-      btn.addEventListener("click", e => { //ボタンが押されたときに作動します
-        e.preventDefault(); //ボタンを一旦無効化します
+    //id="token_submit"のボタンが存在する場合、下記の処理を実行する
+    if (document.getElementById("token_submit") != null) { 
+
+      // 公開鍵の設定
+      Payjp.setPublicKey("pk_test_647e9e364c53fd7162520df4");
+      
+      // id="token_submit"のボタンを取得
+      let btn = document.getElementById("token_submit"); 
+      
+      // ボタンが押下された時に実行される処理
+      btn.addEventListener("click", e => { 
+
+        // デフォルトの挙動を抑止
+        e.preventDefault();
+
+        // カード情報（入力値）の設定
         let card = {
           number: document.getElementById("card_number").value,
           cvc: document.getElementById("cvc").value,
           exp_month: document.getElementById("exp_month").value,
           exp_year: document.getElementById("exp_year").value
-        }; //入力されたデータを取得します。
+        }; 
+
+        // トークンを作成する
         Payjp.createToken(card, (status, response) => {
-          if (status === 200) { //成功した場合
+
+          // 成功した場合
+          if (status === 200) { 
+            //データを自サーバにpostしないように削除（モデルと紐付けされないようにする）
             $("#card_number").removeAttr("name");
             $("#cvc").removeAttr("name");
             $("#exp_month").removeAttr("name");
-            $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
+            $("#exp_year").removeAttr("name"); 
+
+            // フォーム内に隠し項目を追加し、response.id（=トークン）をvalueに設定
+              // payjp-tokenをパラメータとして、サーバー側へ送信する
             $("#card_token").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
-            ); //取得したトークンを送信できる状態にします
+            );
+
+            // サブミット処理を実行する
             document.inputForm.submit();
-            alert("登録が完了しました"); //確認用
+
+            // 成功アラート
+            alert("登録が完了しました"); 
           } else {
-            alert("カード情報が正しくありません。"); //確認用
+            
+            // トークン作成が失敗した場合
+            alert("カード情報が正しくありません。"); 
           }
         });
       });
