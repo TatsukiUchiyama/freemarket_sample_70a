6a2410faa431c5ac32dd172f15f782106318620e
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-03-11 12:03:07 +0900

    修正箇所

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   def index
     @product = Product.find(1)
     @products = Product.includes(:images, :category, :user)
-    # @ham = Product.where(category_id: 2)
     @ham = Product.where(brand: '伊藤ハム')
   end
 
