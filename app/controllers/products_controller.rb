5c23f2d1a73dfc10e916fb53d3ef82324877554a
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-03-11 10:56:37 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.find(1)
+    @products = Product.includes(:images, :category, :user)
     # @ham = Product.where(category_id: 2)
     @ham = Product.where(brand: '伊藤ハム')
   end
