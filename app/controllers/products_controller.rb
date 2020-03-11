f5201ecf416e70ead4f3468dd4ceb16802d380bb
higasimaru <andyourbirdcansing@icloud.com>
2020-03-12 00:08:22 +0900

    カテゴリー表示、ビュー修正
    画像投稿枚数1or2に対応

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @user = @product.user
-    @parents = Category.all.order("id ASC").limit(13)
+    @category = @product.category
   end
 
   def edit
