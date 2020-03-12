68f4e0a24ce634bd436fc6ba7dfb940474520617
NakajimaDctr <drcardhms@gmail.com>
2020-03-12 13:56:00 +0900

    テストコード作成

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,5 +3,5 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :card
 
-  validates :product_id, :user_id, :card_id, presence: true
+  validates :product_id, :user_id, :card_id, :status, presence: true
 end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :category do
+    id       {1}
+    name     {"テストカテゴリー"}
+    ancestry {nil}
+  end
+
+end
