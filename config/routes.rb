a7068b08d931781746b70b48a7a8868336569f31
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-03-16 21:10:29 +0900

    カテゴリーページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :users, only: :show do
     resources :cards, only: [:new, :create]
   end
+  resources :categories, only: :show
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_032003) do
+ActiveRecord::Schema.define(version: 2020_03_13_122658) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -38,12 +38,11 @@ ActiveRecord::Schema.define(version: 2020_03_16_032003) do
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -74,7 +73,6 @@ ActiveRecord::Schema.define(version: 2020_03_16_032003) do
     t.text "description", null: false
     t.string "brand"
     t.integer "price", null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
@@ -82,6 +80,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_032003) do
     t.integer "shipping_from_area_id", null: false
     t.integer "shipping_duration_id", null: false
     t.integer "condition_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -129,9 +128,7 @@ ActiveRecord::Schema.define(version: 2020_03_16_032003) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "cards", "users"
   add_foreign_key "images", "products"
-  add_foreign_key "products", "users"
   add_foreign_key "transactions", "products"
   add_foreign_key "transactions", "users"
 end
