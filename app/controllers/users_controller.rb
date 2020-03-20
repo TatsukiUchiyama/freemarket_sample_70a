396511ff0df490b6dada0f001b09377d0b51e17a
NakajimaDctr <drcardhms@gmail.com>
2020-03-20 15:41:43 +0900

    カード情報を表示（未登録の場合、登録ボタン表示）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,18 @@ class UsersController < ApplicationController
   end
   
   def card
-    
-  end
+    @card = Card.find_by(user_id: current_user.id)
 
+    # 現在のユーザーがカードを登録済みの場合、カードの情報（payjp）を取得する
+    if @card
+      # 秘密鍵を設定
+      Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
+
+      # 所有者を取得
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+
+      # 所有者に紐づくカードIDから、idを指定してカード情報（payjp）を取得する
+      @card_payjp = customer.cards.retrieve(@card.card_id)
+    end
+  end
 end
