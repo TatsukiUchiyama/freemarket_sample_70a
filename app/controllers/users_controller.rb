bbcbe88e9287297ea8c3d6313ed1dd01c3277d2c
NakajimaDctr <drcardhms@gmail.com>
2020-03-20 14:19:36 +0900

    ルーティング設定、アクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_path unless user_signed_in? && current_user.id == @user.id
   end
+  
+  def card
+    
+  end
 
 end
