88654a55ff3df700ad28da5559ab458cb540bc56
NakajimaDctr <drcardhms@gmail.com>
2020-03-20 14:19:36 +0900

    ルーティング設定、アクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   end
   resources :users, only: :show do
     resources :cards, only: [:new, :create]
+    member do
+      get 'card'
+    end
   end
   resources :categories, only: :show
 end
