fe5f75a1748d4d20539451a12c519ad06b945156
sentaku-0303 <yo.hy.mi.06.24@gmail.com>
2020-03-20 20:16:40 +0900

    手数料計算追記

--- app/assets/javascripts/product_new.js
+++ app/assets/javascripts/product_new.js
@@ -4,7 +4,8 @@ $(function(){
     let profit = Math.round(data * 0.9)  
     let fee = (data - profit) 
     $('.sell__main__content__form__box__price__fee__right').html("¥ " + fee) 
-    $('.sell__main__content__form__box__price__profit__right').html("¥ " + profit)
+		$('.sell__main__content__form__box__price__profit__right').html("¥ " + profit)
+		$('#price').val(profit)
     if(data < 300) { 
     $('.sell__main__content__form__box__price__profit__right').html('');
     $('.sell__main__content__form__box__price__fee__right').html('');
