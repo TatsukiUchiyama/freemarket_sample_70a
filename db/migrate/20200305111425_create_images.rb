b009a17d6e14305187e4fb606ba3f92da5032357
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-03-24 14:40:23 +0900

    修正

--- db/migrate/20200305111425_create_images.rb
+++ db/migrate/20200305111425_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.string     :src,       null: false
+      t.string     :image,       null: false
       t.references :product,     null: false, foreign_key: true
       t.timestamps
     end
